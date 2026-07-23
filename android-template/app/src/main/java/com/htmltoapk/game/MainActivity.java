0f552508456ec86be7bcbbf90f909cc4c022015c
ariepplara-glitch <ariepplara@gmail.com>
2026-07-23 19:32:47 +0700

    Update MainActivity.java

--- android-template/app/src/main/java/com/htmltoapk/game/MainActivity.java
+++ android-template/app/src/main/java/com/htmltoapk/game/MainActivity.java
@@ -1,6 +1,7 @@
 package com.agunpu.game;
 
 import android.os.Bundle;
+import android.content.pm.ActivityInfo;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.JavascriptInterface;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // default portrait (game tanpa meta orientation = portrait, misal Neon Dodge)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         webView = findViewById(R.id.webview);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
@@ -22,12 +26,22 @@ public class MainActivity extends AppCompatActivity {
         settings.setAllowContentAccess(true);
         settings.setMediaPlaybackRequiresUserGesture(false);
 
-        // jembatan JS -> native: biar tombol KELUAR di game bisa nutup app
+        // jembatan JS -> native: tombol KELUAR + orientasi otomatis per-game
         webView.addJavascriptInterface(new Object() {
             @JavascriptInterface
             public void exitApp() {
                 runOnUiThread(new Runnable() {
                     @Override public void run() { finish(); }
+                });
+            }
+            @JavascriptInterface
+            public void setOrientation(String mode) {
+                final int o;
+                if ("landscape".equals(mode)) o = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                else if ("portrait".equals(mode)) o = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                else o = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
+                runOnUiThread(new Runnable() {
+                    @Override public void run() { setRequestedOrientation(o); }
                 });
             }
         }, "Android");
